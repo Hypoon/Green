f448d3b01250a32bbac06787a9467263137b0726
Hypoon <futurehypoon@gmail.com>
2016-01-27 01:01:31 -0500

    Initial experimenting with Reflex, Network IO, and super-simple CLI client.

--- src/Client.hs
+++ src/Client.hs
@@ -1,10 +1,14 @@
 import System.IO
-import Logger
+--import Logger
 import Control.Concurrent
 import Control.Concurrent.Chan
 import Data.List
-import Services
+--import Services
+import Control.Monad
+import System.Environment
+import Network
 
+{-
 main = do
     putStrLn "Client"
     (sm::ServiceManager) <- runServiceIO [] (start ())
@@ -45,3 +49,89 @@ waitLoop = do
 waitLoopMessageHandler :: ServiceMsg -> [Service] -> [Service]
 waitLoopMessageHandler (ServiceStarted svc) list = (svc:list)
 waitLoopMessageHandler (ServiceStopped svc) list = (delete svc list)
+-}
+
+getXY :: (Int,Int,Int) -> (Int,Int)
+getXY (u,v,w) = (40+((-2*u)*2),12+(-(v-w)*1))
+
+hexList :: [(Int,Int)]
+hexList = sortOn (snd) $ map getXY $ allList
+
+onScreen :: (Int,Int,Int) -> Bool
+onScreen h = ((fst (getXY h)) >= 3) && ((fst (getXY h)) <= 78) && ((snd (getXY h)) >= 3) && ((snd (getXY h)) <= 23)
+
+allList :: [(Int,Int,Int)]
+allList = filter (onScreen) $ filter (\(u,v,w) -> (u+v+w)==0) [(u,v,w) | u <- [-4..4] , v <- [-4..4], w <- [-4..4]]
+
+drawHexXY :: (Int,Int) -> IO()
+drawHexXY (x,y) = do
+    drawStr (x-1,y-1)   "___"
+    drawStr (x-2,y)    "/   \\"
+    drawStr (x-2,y+1) "\\___/"
+
+drawStr :: (Int,Int) -> String -> IO()
+drawStr (x,y) str = do
+    putStr ("\x1b["++(show y)++";"++(show x)++"H"++str)
+
+drawChar :: (Int,Int) -> Char -> IO()
+drawChar (x,y) c = do
+    drawStr (x,y) (c:[])
+
+drawVLine :: Int -> (Int,Int) -> IO()
+drawVLine x (a,b) = do
+    mapM_ (\h -> drawChar (x,h) '|') [a..b]
+
+drawHLine :: Int -> (Int,Int) -> IO()
+drawHLine y (a,b) = do
+    drawStr (a,y) (replicate (1+b-a) '-')
+
+drawBox :: (Int,Int) -> IO()
+drawBox (width,height) = do
+    hSetEcho stdin False
+    hSetBuffering stdin NoBuffering
+    hSetBuffering stdout NoBuffering
+    putStr "\x1b[2J"
+    putStr "\x1b[?25l"
+    drawChar (1,1) '+'
+    drawChar (1,height) '+'
+    drawChar (width,1) '+'
+    drawChar (width,height) '+'
+    drawVLine 1 (2,height-1)
+    drawVLine width (2,height-1)
+    drawHLine 1 (2,width-1)
+    drawHLine height (2,width-1)
+    return ()
+
+data MoveCommand = MoveCommand Char deriving (Read,Show)
+data NewLocation = NewLocation (Int,Int,Int) deriving (Read,Show)
+
+main = do
+    args <- getArgs
+    let hostname = if (length args == 0)
+        then "127.0.0.1"
+        else (head args)
+    putStrLn "Woo"
+    drawBox (80,25)
+    mapM_ drawHexXY hexList
+    {-let l1 = (map (\h -> drawChar (getXY h) 'U') allList)
+    let l2 = (map (\h -> drawChar (getXY h) ' ') allList)
+    let l3 = repeat (threadDelay 500000)
+    sequence_ $ concat $ (zipWith3 (\a b c -> [a,b,c]) l1 l3 l2) -}
+    net <- connectTo hostname (PortNumber 65008)
+    let loop loc = do
+          drawChar (getXY loc) 'U' 
+          dir <- getChar
+          if (dir /= '\x1b')
+            then do
+              hPutStrLn net (show $ MoveCommand dir)
+              resp <- hGetLine net
+              let (NewLocation newLoc) = read resp
+              drawChar (getXY loc) ' '
+              loop newLoc
+            else do
+              hPutStrLn net (show $ MoveCommand dir)
+              hClose net
+    loop (0,0,0)
+    putStr ("\x1b[25;80H")
+    putStr "\x1b[?25h"
+    putStrLn ""
--- src/StateServer.hs
+++ src/StateServer.hs
@@ -1,4 +1,87 @@
+{-# LANGUAGE RankNTypes #-}
+
 import System.IO
+import Network
+import Control.Monad
+import Control.Concurrent.Chan
+import Control.Concurrent.Async
+import Reflex
+import Control.Monad
+import Data.IORef (readIORef)
+import Reflex.Host.Class (newEventWithTriggerRef, runHostFrame, fireEvents)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.Identity (Identity(..))
+import Data.Dependent.Sum (DSum ((:=>)))
+import Control.Monad.Fix (MonadFix)
+
+data MoveCommand = MoveCommand Char deriving (Read,Show)
+data NewLocation = NewLocation (Int,Int,Int) deriving (Read,Show)
+
+charToDir :: Char -> (Int,Int,Int)
+charToDir 'w' = (0,1,-1)
+charToDir 's' = (0,-1,1)
+charToDir 'q' = (1,0,-1)
+charToDir 'e' = (-1,1,0)
+charToDir 'a' = (1,-1,0)
+charToDir 'd' = (-1,0,1)
+charToDir _ = (0,0,0)
+
+addLoc :: (Int,Int,Int) -> (Int,Int,Int) -> (Int,Int,Int)
+addLoc (a,b,c) (d,e,f) = (a+d,b+e,c+f)
+
+addNewLoc :: NewLocation -> NewLocation -> NewLocation
+addNewLoc (NewLocation a) (NewLocation b) = NewLocation (a `addLoc` b)
+
+processInput :: MoveCommand -> NewLocation
+processInput (MoveCommand c) = NewLocation (charToDir c)
+
+type StateServer t m = (Reflex t, MonadHold t m, MonadFix m) => Event t MoveCommand -> m (Behavior t NewLocation)
+
+host :: (forall t m. StateServer t m) -> Chan MoveCommand -> Chan NewLocation -> IO ()
+host myGuest inputChan outputChan = do
+    runSpiderHost $ do
+        (e, eTriggerRef) <- newEventWithTriggerRef
+        b <- runHostFrame $ myGuest e
+        forever $ do
+            input <- liftIO (readChan inputChan)
+            mETrigger <- liftIO $ readIORef eTriggerRef
+            case mETrigger of
+                Nothing -> do
+                    return ()
+                Just eTrigger -> do
+                    fireEvents [eTrigger :=> input]
+            output <- runHostFrame $ sample b
+            liftIO $ writeChan outputChan output
+
+guest :: StateServer t m
+guest e = do
+    --f <- holdDyn (MoveCommand ' ') e
+    --d <- mapDyn (processInput) f
+    d <- foldDyn (\a b -> addNewLoc (processInput a) b) (NewLocation (0,0,0)) e
+    return $ current d
+
+-- host guest inputChan
+
 
 main = do
     putStrLn "State Server"
+    ssock <- listenOn (PortNumber 65008)
+    (net,_,_) <- accept ssock
+    inputChan <- newChan
+    outputChan <- newChan
+    async (sequence $ repeat $ (hGetLine net >>= (writeChan inputChan . read)))
+    async (sequence $ repeat $ (readChan outputChan >>= (hPutStrLn net . show)))
+    host guest inputChan outputChan
+--    let loop loc = do
+--            incoming <- hGetLine net
+--            let (MoveCommand c) = read incoming
+--            if (c /= '\x1b')
+--              then do
+--                let newLoc = loc `addLoc` (charToDir c)
+--                hPutStrLn net $ show $ NewLocation newLoc
+--                loop newLoc
+--              else do
+--                hClose net
+--                sClose ssock
+--    loop (0,0,0)
+--    return ()
