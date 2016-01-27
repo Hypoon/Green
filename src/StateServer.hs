f1ab974ac8db0de0373b3bedd0df89a742bd8d50
Hypoon <futurehypoon@gmail.com>
2016-01-27 13:57:54 -0500

    Removed unnecessary Chans and Asyncs.

--- src/StateServer.hs
+++ src/StateServer.hs
@@ -3,8 +3,6 @@
 import System.IO
 import Network
 import Control.Monad
-import Control.Concurrent.Chan
-import Control.Concurrent.Async
 import Reflex
 import Control.Monad
 import Data.IORef (readIORef)
@@ -37,13 +35,13 @@ processInput (MoveCommand c) = NewLocation (charToDir c)
 
 type StateServer t m = (Reflex t, MonadHold t m, MonadFix m) => Event t MoveCommand -> m (Behavior t NewLocation)
 
-host :: (forall t m. StateServer t m) -> Chan MoveCommand -> Chan NewLocation -> IO ()
-host myGuest inputChan outputChan = do
+host :: (forall t m. StateServer t m) -> Handle -> IO ()
+host myGuest net = do
     runSpiderHost $ do
         (e, eTriggerRef) <- newEventWithTriggerRef
         b <- runHostFrame $ myGuest e
         forever $ do
-            input <- liftIO (readChan inputChan)
+            input <- liftIO (liftM read $ hGetLine net)
             mETrigger <- liftIO $ readIORef eTriggerRef
             case mETrigger of
                 Nothing -> do
@@ -51,7 +49,7 @@ host myGuest inputChan outputChan = do
                 Just eTrigger -> do
                     fireEvents [eTrigger :=> input]
             output <- runHostFrame $ sample b
-            liftIO $ writeChan outputChan output
+            liftIO $ hPutStrLn net $ show output
 
 guest :: StateServer t m
 guest e = do
@@ -60,18 +58,11 @@ guest e = do
     d <- foldDyn (\a b -> addNewLoc (processInput a) b) (NewLocation (0,0,0)) e
     return $ current d
 
--- host guest inputChan
-
-
 main = do
     putStrLn "State Server"
     ssock <- listenOn (PortNumber 65008)
     (net,_,_) <- accept ssock
-    inputChan <- newChan
-    outputChan <- newChan
-    async (sequence $ repeat $ (hGetLine net >>= (writeChan inputChan . read)))
-    async (sequence $ repeat $ (readChan outputChan >>= (hPutStrLn net . show)))
-    host guest inputChan outputChan
+    host guest net
 --    let loop loc = do
 --            incoming <- hGetLine net
 --            let (MoveCommand c) = read incoming
